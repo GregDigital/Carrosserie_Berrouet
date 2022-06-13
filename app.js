f7d73c7f18c7cd3c98acf4261af4ec4509f2bcf7
GregDigital <68654973+GregDigital@users.noreply.github.com>
2022-06-13 10:45:35 +0200

    js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-
-  new Vivus('svg', { 
+new Vivus(
+  "svg",
+  {
     delay: 0,
-    duration: 120
-},
-function (obj) {
-	obj.el.classList.add("finished")
-}
+    duration: 120,
+  },
+  function (obj) {
+    obj.el.classList.add("finished");
+  }
 );
+
+var controller = new ScrollMagic.Controller();
+new ScrollMagic.Scene({
+  triggerElement: "#trigger1",
+  triggerHook: 0.9, // show, when scrolled 10% into view
+  duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+  offset: 50, // move trigger to center of element
+})
+  .setClassToggle("#reveal1", "visible") // add class to reveal
+  .addIndicators({ name: "GSAP" }) // add indicators (requires plugin) // add indicators (requires plugin)
+  .addTo(controller);
