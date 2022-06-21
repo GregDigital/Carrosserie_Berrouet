7d4cf62b212627a4cb255d30b758562d64329dcf
GregDigital <68654973+GregDigital@users.noreply.github.com>
2022-06-21 20:13:48 +0200

    modif

--- data.js
+++ data.js
@@ -1,31 +1,64 @@
-let container = document.querySelector(".lightbox");
+let container = document.querySelector(".cont");
+let items = document.querySelectorAll(".item");
 
 function show(response) {
   let acc = [];
-  for (let img of response.golf) {
-    acc.push(img);
-    console.log(img);
+  for (let img of response.car) {
+    acc.push(lightbox(img));
   }
-  let after = acc.img.after;
-  console.log(after);
+  let html = acc.reduce((a, l) => a + l);
+  container.innerHTML = html;
+  afficher(response.car);
+  light();
+}
+
+function afficher(user) {
+  items.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      if (user.id == parent) {
+        console.log(parent);
+      }
+    });
+  });
 }
 
 function lightbox(user) {
+  console.log("ok");
   return `
-    <div class="lightbox">
-    <div class="slider ">
-      <div class="slider__before"></div>
-      <div class="slider__separator"></div>
-      <div class="slider__after"></div>
+        <div class="image-container">
+        <img
+          class="image-before slider-image"
+         src="../assets/img/${user.before}"
+          alt="color photo"
+        />
+        <img
+          class="image-after slider-image"
+          src="../assets/img/${user.after}"
+          alt="black and white"
+        />
+      </div>
+      <!-- step="10" -->
       <input
-        class="slider__range"
         type="range"
         min="0"
         max="100"
         value="50"
+        aria-label="Percentage of before photo shown"
+        class="slider"
       />
-    
-    `;
+      <div class="slider-line" aria-hidden="true"></div>
+      <div class="slider-button" aria-hidden="true">
+      
+      
+      </div>
+          `;
+}
+
+function light() {
+  const conta = document.querySelector(".cont");
+  document.querySelector(".slider").addEventListener("input", (e) => {
+    conta.style.setProperty("--position", `${e.target.value}%`);
+  });
 }
 
 fetch("afterbefore.json")
