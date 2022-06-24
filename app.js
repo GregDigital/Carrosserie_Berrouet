0003e5c7ab13380ee62d91e1cf276279a045d615
GregDigital <68654973+GregDigital@users.noreply.github.com>
2022-06-24 19:17:54 +0200

    responsive design

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Animation lettre bienvenue
+
 new Vivus(
   "svg",
   {
@@ -8,6 +10,8 @@ new Vivus(
     obj.el.classList.add("finished");
   }
 );
+
+// Animation scroll reveal
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -45,3 +49,43 @@ masks.forEach((mask) => {
     opacity: 1,
   });
 });
+
+// Menu burger responsive
+
+let burgerMenu = document.querySelector(".menu_toggle");
+let overlay = document.querySelector(".t");
+let body = document.querySelector("body");
+let tl = gsap.timeline();
+
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("active");
+  overlay.classList.toggle("overlay");
+  body.classList.toggle("true");
+
+  if (this.classList.contains("active")) {
+    tl.from("li", {
+      y: 40,
+      opacity: 0,
+      stagger: 0.2,
+    });
+  } else {
+    tl.from("li", {
+      opacity: 1,
+    });
+  }
+});
+
+let menu = document.querySelectorAll("li");
+
+for (let i = 0; i < menu.length; i++) {
+  const el = menu[i];
+  el.addEventListener("click", function () {
+    overlay.classList.toggle("overlay");
+    burgerMenu.classList.toggle("active");
+    body.classList.toggle("true");
+
+    tl.from("li", {
+      opacity: 1,
+    });
+  });
+}
