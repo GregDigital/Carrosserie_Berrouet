6cf88f4e5e08ce64bf2f1949ad2b2a5bf362fbab
GregDigital <68654973+GregDigital@users.noreply.github.com>
2022-11-30 08:36:24 +0100

    form.php

--- form.php
+++ form.php
@@ -1,1 +1,34 @@
-<?php echo 'Hello, World!'; ?>
+
+<?php
+  $name = valid_donnees($_POST['name']);
+  $first = valid_donnees($_POST['first']);
+  $mobile = valid_donnees($_POST['mobile']);
+  $email = valid_donnees($_POST['email']);
+  $message = valid_donnees($_POST['message']);
+  $picture = valid_donnees($_FILES['picture']);
+
+  function valid_donnees($donnees){
+    $donnees = trim($donnees);
+    $donnees = stripslashes($donnees);
+    $donnees = htmlspecialchars($donnees);
+    return $donnees;
+}
+
+  if(!empty($email) && !empty($message)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $receiver = "hello@sandrapizel.fr";
+      $subject = "From: $name <$email>";
+      $body = "Name: $name\nFirstname: $prenom\nEmail: $email\nMessage:\n$message\n";
+      $sender = "From: $email";
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Merci, Votre message a bien été envoyé";
+      }else{
+         echo "Désolé votre message n'a pas été envoyé";
+      }
+    }else{
+      echo "Merci d'entrer un email valide (contact@mail.fr)";
+    }
+  }else{
+    echo "Merci d'indiquer votre email et votre message";
+  }
+?>
